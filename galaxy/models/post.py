ed28483c4741d4d325dfa58567c3e23b01520abf
huming <100huming@gmail.com>
2019-12-01 01:21:24 +0800

    modify model

--- galaxy/models/post.py
+++ galaxy/models/post.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
     content = db.Column(db.Text, nullable=False)
